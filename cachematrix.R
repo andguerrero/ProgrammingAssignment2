df6e8eda86e18bf4460993b0a89ff8e4c3fe0d1c
andguerrero <andre.guerrero+github@outlook.com>
2014-07-27 02:35:44 -0700

    Solution Push for Programming Assignment 2
    Solution Push for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
-
+#The makeCacheMatrix function
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
+#The cacheSolve function
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
